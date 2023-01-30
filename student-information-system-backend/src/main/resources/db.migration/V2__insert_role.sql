5378d8e1c83f69b90648e6e770a73ec4ee4cdd6a
trkgrn <takoyaso58@gmail.com>
2023-01-30 22:49:36 +0300

    admin added

--- student-information-system-backend/src/main/resources/db.migration/V2__insert_role.sql
+++ student-information-system-backend/src/main/resources/db.migration/V2__insert_role.sql
@@ -5,3 +5,7 @@ VALUES (1, 'STUDENT');
 INSERT INTO public.role(
     role_id, name)
 VALUES (2, 'TEACHER');
+
+INSERT INTO public.role(
+    role_id, name)
+VALUES (3, 'ADMIN');
---
+++ student-information-system-backend/src/main/resources/db.migration/V3__insert_admin.sql
@@ -0,0 +1,17 @@
+INSERT INTO public."user"
+    (first_name,
+     last_name,
+     tck_no,
+     tel_no,
+     e_mail,
+     address,
+     password,
+     role_id)
+    VALUES ('Root',
+            'Admin',
+            '11111111111',
+            '1111111111',
+            'admin@admin.com',
+            'Admin/Admin',
+            '$2a$12$GzqFuBDFJdZUCvAT8GAA.enwehHJjeIOA1Add4S0G/Mhv2xsJxJZO',
+            3);
